dbe2af8abd2a54a61b11a53f23425a7229c0f08b
Ded_Boi <dhanush911gtr@gmail.com>
2018-06-16 03:43:28 +0000

    op3: dot-ify
    Signed-off-by: Ded_Boi <dhanush911gtr@gmail.com>

--- dot.mk
+++ dot.mk
@@ -20,10 +20,10 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from oneplus3 device
 $(call inherit-product, device/oneplus/oneplus3/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common dotOS stuff.
+$(call inherit-product, vendor/dot/config/common.mk)
 
-PRODUCT_NAME := lineage_oneplus3
+PRODUCT_NAME := dot_oneplus3
 PRODUCT_DEVICE := oneplus3
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
